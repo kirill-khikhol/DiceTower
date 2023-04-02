aed3d886f499cee197310f80c3d8b3fd896ea00e
kir.khi <garry4ever@gmail.com>
2023-04-02 13:20:44 +0300

    tower prefabs added

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dice : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Dice:MonoBehaviour {
+    private const int STOP_COUNT = 5;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField] private Rigidbody _rigidbody;
+    private int _stopCount;
+    private bool isCounted = false;
+
+
+    private void FixedUpdate() {
+        if (!isCounted && _rigidbody.velocity == Vector3.zero) {
+            if (_stopCount < STOP_COUNT) {
+                _stopCount++;
+            } else {
+                isCounted = true;
+                Debug.Log("Count me");
+            }
+        }
+
+      
+
     }
 }
